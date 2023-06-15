43d1b7612ccdec7ed5a4f4abe291d35c72c1fba8
mateuszkor <mateuszkor5006@gmail.com>
2023-06-15 21:10:09 +0200

    Update the footer with fiver link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
+import React from 'react';
+
 function Footer() {
+  const linkStyle = {
+    color: 'rgb(234, 179, 8)',
+  };
+
   return (
     <div className="py-5 text-center">
       <p className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
         Vite + React + Tailwind. This stack is a definite overkill but go big or
-        go home right...?
+        go home right...? If you are interested, I can build similar styled
+        website for you
+        <a
+          href="https://www.fiverr.com/s/oL0xzG"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={linkStyle}
+        >
+          {' '}
+          here
+        </a>
+        .
       </p>
       <p className="text-sm mt-2  opacity-50">
         &copy; {new Date().getFullYear()} Mateusz Korytkowski. All rights
